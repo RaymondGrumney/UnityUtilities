aa0b37628cfdf2dbc79a2a6a81596a8a560ead93
Raymond Grumney <Raymond.Grumney@gmail.com>
2020-02-17 07:19:01 -0800

    Destroys object when it has not child objects in the hierarchy

---
+++ CommonAssets/DestroyWhenNoChildren.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace CommonAssets.Utilities
+{
+    public class DestroyWhenNoChildren : MonoBehaviour
+    {
+        private void Update()
+        {
+            if(transform.childCount == 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
