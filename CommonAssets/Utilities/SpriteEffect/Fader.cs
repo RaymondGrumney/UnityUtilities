35890eb267b4e4e464ff569295dcfc862789420d
Raymond Grumney <Raymond.Grumney@gmail.com>
2020-02-17 09:33:37 -0800

    Fades a sprite in or out

---
+++ CommonAssets/Utilities/SpriteEffect/Fader.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace CommonAssets.Utilities
+{
+    public class Fader
+    {
+        // TODO: Enable Fade In
+        // TODO: Sprite Ocillation
+        // TODO: Remove script after finished fading in/out
+
+        private GameObject _gameObject;
+        private float _fadeOutTimeInSeconds = 1f;
+        private bool _thenDestroy = false;
+        private bool _fadeOut = false;
+        private bool _fadeIn = false;   // untested
+        private int _modifier = -1;
+        private float _minAlpha = 0f;   // checked but not settable
+        private float _maxAlpha = 1f;   // not used yet
+        private FaderComponent component;
+
+        public Fader(GameObject gameObject)
+        {
+            _gameObject = gameObject;
+            component = gameObject.AddComponent<FaderComponent>();
+            component.StartCoroutine(Initialize());
+        }
+
+        public Fader() 
+        {
+        }
+
+        public Fader Fade(GameObject gameObject)
+        {
+            _gameObject = gameObject;
+            return this;
+        }
+
+        public Fader Over(float nSeconds)
+        {
+            _fadeOutTimeInSeconds = nSeconds;
+            return this;
+        }
+
+        public Fader Destroy()
+        {
+            _thenDestroy = true;
+            return this;
+        }
+
+        public Fader Out()
+        {
+            _fadeOut = true;
+
+            if(!_fadeIn)
+            {
+                _modifier = -1;
+            }
+
+            return this;
+        }
+
+        public Fader In()
+        {
+            throw new NotImplementedException("It probably works but hasn't been tested.");
+
+            _fadeIn = true;
+
+            if(!_fadeOut)
+            {
+                _modifier = 1;
+            }
+
+            return this;
+        }
+
+        public Fader Then => this;
+
+        private IEnumerator Initialize()
+        {
+            yield return new WaitForEndOfFrame();
+
+            if (!_fadeOut && !_fadeIn)
+            {
+                throw new ArgumentException("In() and/or Out() must be called to define behavior.");
+            }
+            
+            component.fadeOutTimeInSeconds  = _fadeOutTimeInSeconds;
+            component.thenDestroy           = _thenDestroy;
+            component.fadeIn                = _fadeIn;   // untested
+            component.fadeOut               = _fadeOut;
+            component.modifier              = _modifier;
+            component.minAlpha              = _minAlpha; // currently checked but not setable
+            component.maxAlpha              = _maxAlpha; // currently unchecked
+        }
+    }
+}
---
+++ CommonAssets/Utilities/SpriteEffect/FaderComponent.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaderComponent : MonoBehaviour {
+
+	/// <summary>
+	/// The sprite renderer to fade out
+	/// </summary>
+	protected SpriteRenderer _spriteRenderer;
+
+	/// <summary>
+	/// How long it takes to fade the sprite out.
+	/// </summary>
+	[Tooltip("How long it takes to fade the sprite out.")]
+	public float fadeOutTimeInSeconds = 1f;
+
+	/// <summary>
+	/// Whether to destroy the object at end of fade.
+	/// </summary>
+	[Tooltip("Whether to destroy the object at end of fade.")]
+	public bool thenDestroy = false;
+
+	/// <summary>
+	/// Whether the sprite fades out.
+	/// </summary>
+	[Tooltip("Whether the sprite fades out.")]
+	public bool fadeOut = false;
+
+	/// <summary>
+	/// Whether the sprite fades in.
+	/// </summary>
+	[Tooltip("Whether the sprite fades in.")]
+	public bool fadeIn = false;
+
+	/// <summary>
+	/// The direction the alpha is being modified.
+	/// </summary>
+	[Tooltip("The direction the alpha is currently being modified.")]
+	public int modifier = -1;
+
+	/// <summary>
+	/// The minimum alpha (transparency).
+	/// </summary>
+	[Tooltip("The minimum alpha (transparency).")]
+	public float minAlpha = 0f;
+
+	/// <summary>
+	/// The maximum alpha (transparency).
+	/// </summary>
+	[Tooltip("The maximum alpha (transparency).")]
+	public float maxAlpha = 1f;
+
+
+	// Use this for initialization
+	void Start () {
+		_spriteRenderer = GetComponent<SpriteRenderer>();
+	}
+
+	private void FixedUpdate()
+	{
+		float r = _spriteRenderer.color.r;
+		float g = _spriteRenderer.color.g;
+		float b = _spriteRenderer.color.b;
+		float a = _spriteRenderer.color.a + (Time.fixedDeltaTime / fadeOutTimeInSeconds * modifier);
+		_spriteRenderer.color = new Color(r, g, b, a);
+
+		// this checks if faded out to minimal alpha
+		if (modifier == -1) // Fading out
+		{
+			if (_spriteRenderer.color.a <= minAlpha)
+			{
+				a = minAlpha;
+				_spriteRenderer.color = new Color(r, g, b, a);
+	
+				if(thenDestroy)
+				{
+					Destroy(gameObject);
+				}
+				else
+				{
+					if (fadeIn)
+					{
+						modifier *= -1;
+					}
+				}
+			}
+		}
+		else if ( modifier == 1 ) // Fading in
+		{
+			// TODO: Fade in
+
+			if (_spriteRenderer.color.a <= minAlpha)
+			{
+				a = minAlpha;
+				_spriteRenderer.color = new Color(r, g, b, a);
+
+				if (thenDestroy)
+				{
+					Destroy(gameObject);
+				}
+				else
+				{
+					if(fadeOut)
+					{
+						modifier *= -1;
+					}
+				}
+			}
+		}
+		else // modifier == 0?
+		{
+			// TODO: pause between fade in/out cycles?
+			throw new ArgumentNullException("Modifier unset.");
+		}
+	}
+}
