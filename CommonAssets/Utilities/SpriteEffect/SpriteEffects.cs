bf93cfc6d001f09ce690d26e0ab1bdac5b3bbc60
Raymond Grumney <Raymond.Grumney@gmail.com>
2020-02-17 09:34:31 -0800

    Static Class to access Sprite Effects, such as Explode or Fade Out

---
+++ CommonAssets/Utilities/SpriteEffect/SpriteEffects.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace CommonAssets.Utilities
+{
+    /// <summary>
+    /// Applys effects to sprites
+    /// </summary>
+    public class SpriteEffects
+    {
+        /// <summary>
+        /// Explodes the sprite
+        /// </summary>
+        /// <param name="sprite"></param>
+        /// <returns></returns>
+        public static Exploder Explode(Sprite sprite)
+            => new Exploder(sprite);
+
+        /// <summary>
+        /// Fades the sprite in or out (in currently not enabled)
+        /// </summary>
+        /// <param name="gameObject"></param>
+        /// <returns></returns>
+        public static Fader Fade(GameObject gameObject)
+            => new Fader(gameObject);
+
+        // TODO: cut in half
+    }
+}
