80d46dd39edf7f49ee82770b9b75814db8e96af2
Raymond Grumney <34448933+RaymondGrumney@users.noreply.github.com>
2020-02-28 12:49:26 -0800

    Destroys when parent enters a trigger with a particular script on it

---
+++ CommonAssets/DestroyOnOtherScriptTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class DestroyOnOtherScriptTrigger : MonoBehaviour
+{
+    public MonoScript DestructiveScript;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.GetComponent(DestructiveScript.name))
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
