0d10be005d97f788cfce6383fa715fb23ed370e2
Raymond Grumney <Raymond.Grumney@nexplore.com>
2021-09-18 09:28:43 -0700

    BUGFIX: New coroutines replaced old coroutines

--- UnityUtilities/Utilities/Easily/CoroutineRunner.cs
+++ UnityUtilities/Utilities/Easily/CoroutineRunner.cs
@@ -12,12 +12,12 @@ namespace UnityUtilities.Utilities
 
         public static CoroutineRunner Init(IEnumerator coroutine = null, Action action = null ) 
         {
+            // if (action !=null) Debug.Log($"Init({coroutine}, {action}), {action?.Target}");
             GameObject coroutineObj = GameObject.Find("Coroutines");
             CoroutineRunner coroutineRunner;
             if (coroutineObj) 
             {
-                coroutineRunner = coroutineObj.GetComponent<CoroutineRunner>() 
-                               ?? coroutineObj.AddComponent<CoroutineRunner>();
+                coroutineRunner = coroutineObj.AddComponent<CoroutineRunner>();
                 // if (coroutineRunner is null) {
                 //     coroutineRunner = coroutineObj.AddComponent<CoroutineRunner>();
                 // }
@@ -40,30 +40,38 @@ namespace UnityUtilities.Utilities
 
         public CoroutineRunner After(float n) 
         {
+            // if (_action !=null) Debug.Log($"After({n}), {_action?.Target}");
+            _n = n;
             return this;
         }
 
         public void Seconds() 
         {
-            StartCoroutine(_wait());
+            if (_action !=null) Debug.Log($"Seconds, {_action?.Target}");
+            StartCoroutine("_wait");
         }
 
         private IEnumerator _wait()
         {
+            // if (_action !=null) Debug.Log($"_wait({_n}), {_action?.Target}");
             yield return new WaitForSecondsRealtime(_n);
+            // if (_action !=null) Debug.Log($"waited {_n}, {_action?.Target}");
             _do();
         }
 
         private void _do() 
         {
+            // if (_action !=null) Debug.Log($"_do() {_action?.Target}, {_action?.Target}");
             if(_coroutine != null) 
             {
                 StartCoroutine(_coroutine);
             }
             else 
             {
+                // if (_action !=null) Debug.Log($"({_action?.Target}), {_action?.Target}");
                 _action();
             }
+            Destroy(this);
         }
     }
 }
