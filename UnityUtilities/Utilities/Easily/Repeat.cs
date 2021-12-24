70efeef1dc8535048ff336274f04e25baf5b2905
Raymond Grumney <Raymond.Grumney@nexplore.com>
2021-12-24 13:24:27 -0800

    Add Delay option between iterations

--- UnityUtilities/Utilities/Easily/Repeat.cs
+++ UnityUtilities/Utilities/Easily/Repeat.cs
@@ -5,18 +5,28 @@ namespace UnityUtilities.Utilities
     public class Repeat
     {
         private Action _action;
+        private float _delay = 0f;
 
         public Repeat(Action action)
         {
             _action = action;
         }
 
-        public void ThisManyTimes(int loops)
+        public Repeat Delaying(float delay) 
         {
+            _delay=delay;
+            return this;
+        }
+
+        public Repeat ThisManyTimes(int loops)
+        {
+            float d = _delay;
             for (int i = 0; i < loops; i++)
             {
-                _action();
+                Easily.StartCoroutine( _action ).After(d).Seconds();
+                d += _delay;
             }
+            return this;
         }
     }
 }
