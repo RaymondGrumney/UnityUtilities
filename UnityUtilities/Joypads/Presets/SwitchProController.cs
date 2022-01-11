cfcf0e5139503383932790b1733849ea34567b19
Raymond Grumney <raymond.grumney@gmail.com>
2022-01-11 13:11:08 -0800

    SwitchJoypad => SwitchProController

---
+++ UnityUtilities/Joypads/Presets/SwitchProController.cs
@@ -0,0 +1,30 @@
+// using UnityUtilities.Utilities;
+// using System;
+// using System.Collections.Generic;
+// using System.Text;
+// using UnityEngine;
+
+// namespace UnityUtilities.Joypads.Presets
+// {
+//     class XInputJoypad : JoypadPreset
+//     {
+//         string JoypadPreset.jumpKey { get => "JoystickButton0"; }
+//         string JoypadPreset.attackKey { get => "JoystickButton2"; }
+//         string JoypadPreset.shield { get => "righttrigger"; }
+//         string JoypadPreset.interactKey { get => "JoystickButton2"; }
+//         string JoypadPreset.magicKey { get => "JoystickButton3"; }
+//         string JoypadPreset.cyclemagicleft { get => "JoystickButton4"; }
+//         string JoypadPreset.cyclemagicright { get => "JoystickButton5"; }
+//         string JoypadPreset.itemKey { get => "JoystickButton1"; }
+//         string JoypadPreset.upKey { get => "up"; }
+//         string JoypadPreset.downKey { get => "down"; }
+//         string JoypadPreset.leftKey { get => "left"; }
+//         string JoypadPreset.rightKey { get => "right"; }
+//         string JoypadPreset.horizontalAxis { get => "horizontal"; }
+//         string JoypadPreset.verticalAxis { get => "vertical"; }
+//         string JoypadPreset.menuKey { get => "JoystickButton9"; }
+//         string JoypadPreset.menuConfirmKey { get => "JoystickButton0"; }
+//         string JoypadPreset.menuCancelKey { get => "JoystickButton1"; }
+//         string JoypadPreset.menuExitKey { get => "JoystickButton9"; }
+//     }
+// }
